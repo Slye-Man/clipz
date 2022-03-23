c7d8fdb25b89571d0719899ba478cc4561d16b26
Slye-Man <selaelosebola67@gmail.com>
2022-03-23 22:08:28 +0200

    implemented delayed operator by 1000ms and added signout/logout functionality

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public modal: ModalService,
-    public auth: AuthService
+    public auth: AuthService,
+    private afAuth: AngularFireAuth
     ) { }
 
   ngOnInit(): void {
@@ -25,4 +27,9 @@ export class NavComponent implements OnInit {
     this.modal.toggleModal('auth')
   }
 
+  async logout($event: Event) {
+    $event.preventDefault()
+
+    this.afAuth.signOut()
+  }
 }
