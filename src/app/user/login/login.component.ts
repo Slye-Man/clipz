3ce6e923ab3e6e02d154ca6182139ae7ed091a2e
Slye-Man <selaelosebola67@gmail.com>
2022-03-23 21:48:18 +0200

    added functionality to login form and fixed memory leak caused by the modal

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,37 @@ export class LoginComponent implements OnInit {
     email: '',
     password: ''
   }
+  showAlert = false
+  alertMsg = 'Please wait! We are logging you in.'
+  alertColor = 'blue'
+  inSubmission = false
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
 
-  login() {
+  async login() {
+    this.showAlert = true
+    this.alertMsg = 'Please wait! We are logging you in.'
+    this.alertColor = 'blue'
+    this.inSubmission = true
     
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email, this.credentials.password
+      )
+    } catch (error) {
+      this.inSubmission = false
+      this.alertMsg = 'An unexpected error ocurred. Please try again later'
+      this.alertColor = 'red'
+
+      console.log(error); //for debugging purposes
+      return
+    }
+
+    this.alertMsg = 'Success! You are now logged in'
+    this.alertColor = 'green'
   }
 
 }
