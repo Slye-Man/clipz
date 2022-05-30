e59407c222b16564968f6728ddc971eaba67d97b
Selaelo Sebola <selaelosebola67@gmail.com>
2022-05-30 23:58:54 +0200

    Prepared the modal form. Binding the edit form with the modal and updating clip functionality added.

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,21 +1,79 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  OnDestroy, 
+  Input, 
+  OnChanges } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
+import IClip from 'src/app/models/clip.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, OnDestroy {
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
+  @Input() activeClip: IClip | null = null
+  inSubmission = false
+  showAlert = false
+  alertColor = 'blue'
+  alertMsg = 'Please wait! Updating Clip'
 
-  constructor(private moodal: ModalService) { }
+  clipID = new FormControl('')
+  title = new FormControl('', [
+  Validators.required,
+  Validators.minLength(3)
+])
+editForm = new FormGroup({
+  title: this.title,
+  id: this.clipID
+})
+
+  constructor(
+    private modal: ModalService,
+    private clipService: ClipService 
+    ) { }
 
   ngOnInit(): void {
-    this.moodal.register('editClip')
+    this.modal.register('editClip')
   }
 
   ngOnDestroy(): void {
-      this.moodal.unregister('editClip')
+      this.modal.unregister('editClip')
+  }
+
+  ngOnChanges(): void {
+      if(!this.activeClip) {
+        return
+      }
+
+      this.clipID.setValue(this.activeClip.docID)
+      this.title.setValue(this.activeClip.title)
+  }
+
+  async submit() {
+    this.inSubmission = true
+    this.showAlert = true
+    this.alertColor = 'blue'
+    this.alertMsg = 'Please wait! Updating Clip.'
+
+    try {
+    await this.clipService.updateClip(
+      this.clipID.value, this.title.value
+      )
+    }
+      catch(e) {
+        this.inSubmission = false
+        this.alertColor = 'red'
+        this.alertMsg = 'Something went wrong. Try again later!'
+        return
+      }
+
+      this.inSubmission = false
+      this.alertColor = 'green'
+      this.alertMsg = 'Success!'
   }
 
 }
